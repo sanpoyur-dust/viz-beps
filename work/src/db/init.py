7a9d84dff265b916a528ef066ea9bd2bc499dc8c
Jack Xu <yurusanP@gmail.com>
2023-02-24 20:51:14 +0000

    feat: sqlite3 -> psycopg2

--- work/src/db/init.py
+++ work/src/db/init.py
@@ -1,52 +1,75 @@
 import os
-import sqlite3
+import psycopg2
 
 
-col_to_ix = dict.fromkeys(
-    [
-        "rep",
-        "sample",
-        "guideID",
-        "Aligned_Sequence",
-        "Read_Status",
-        "Reference_Sequence",
-        "deletion_positions",
-        "deletion_sizes",
-        "insertion_positions",
-        "insertion_sizes",
-        "substitution_positions",
-        "substitution_values",
-        "n_deleted",
-        "n_inserted",
-        "n_mutated",
-        "Reads",
-    ]
-)
+col_to_ty = {
+    "rep": "TEXT",
+    "sample": "TEXT",
+    "guideid": "TEXT",
+    "aligned_sequence": "TEXT",
+    "read_status": "TEXT",
+    "reference_sequence": "TEXT",
+    "deletion_positions": "INTEGER ARRAY",
+    "deletion_sizes": "INTEGER ARRAY",
+    "insertion_positions": "INTEGER ARRAY",
+    "insertion_sizes": "INTEGER ARRAY",
+    "substitution_positions": "INTEGER ARRAY",
+    "substitution_values": "CHARACTER ARRAY",
+    "n_deleted": "INTEGER",
+    "n_inserted": "INTEGER",
+    "n_mutated": "INTEGER",
+    "reads": "NUMERIC"
+}
+col_to_ix = dict.fromkeys(col_to_ty.keys())
+keys = ", ".join(col_to_ix.keys())
 
 
 def create_table(cur):
-    schema = ", ".join([col + " TEXT" for col in col_to_ix.keys()])
-    cur.execute(f"DROP TABLE IF EXISTS K562;")
-    cur.execute(f"CREATE TABLE K562({schema});")
-
-
+    schema = ", ".join([f"{col} {ty}" for col, ty in col_to_ty.items()])
+    cur.execute(f"DROP TABLE IF EXISTS k562;")
+    cur.execute(f"CREATE TABLE k562(id BIGSERIAL PRIMARY KEY, {schema});")
+    
+    
 def insert_data(cur):
+    def normalize_data(data, ty):
+        data = data.strip()
+        if ty == "TEXT":
+            return f"'{data}'"
+        elif ty == "INTEGER ARRAY":
+            data = data.replace("[", "{").replace("]", "}")
+            return f"'{data}'"
+        elif ty == "CHARACTER ARRAY":
+            data = data.replace("[", "").replace("]", "")
+            data = "{" + (", ".join([chr(int(e)) for e in data.split(" ")]) if data else "") + "}"
+            return f"'{data}'"
+        else:
+            return data
+    
     with open("assets/alleleTables/K562-alleleTable.txt", "r") as f:
-        cols = f.readline().strip().split("\t")
+        cols = f.readline().strip().split("\t")            
         for ix, col in enumerate(cols):
-            if col == "%Reads":
-                col = "Reads"
+            col = col.lower().replace("%", "")
             if col in col_to_ix:
                 col_to_ix[col] = ix
+                
+        ty_and_ix = list(zip(col_to_ty.values(), col_to_ix.values()))
+        
         for line in f:
             cols = line.strip().split("\t")
-            values = ", ".join(["'" + cols[ix] + "'" for ix in col_to_ix.values()])
-            cur.execute(f"INSERT INTO K562 VALUES({values});")
+            values = ", ".join([normalize_data(cols[ix], ty) for ty, ix in ty_and_ix])
+            cur.execute(f"INSERT INTO k562 ({keys}) VALUES({values});")
 
 
 if __name__ == "__main__":
     os.chdir("../..")
-    with sqlite3.connect("assets/alleleTables/K562-alleleTable.db.tmp") as con:
+    with psycopg2.connect(
+        host="localhost",
+        port=9432,
+        dbname="postgres",
+        user="postgres",
+        password="madison",
+    ) as con:
         cur = con.cursor()
         create_table(cur)
         insert_data(cur)
+        
